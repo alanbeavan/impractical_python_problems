3fd4779554cf34d1191283da168ccbdfdfb9b8da
Alan Beavan <alan.beavan@bristol.ac.uk>
2019-08-27 19:30:29 +0100

    2

--- 2/projects/recursive_approach/recursive_palindromes.py
+++ 2/projects/recursive_approach/recursive_palindromes.py
@@ -3,21 +3,25 @@
 
 import load_dictionary
 
-word_list = sorted(set(load_dictionary.load('../../dictionary.txt')))
 
 def pali_check(string):
+    """Recursively returns 1 if string is palindrome."""
     if len(string) <= 1:
         return 1
-    elif string[0] == string[-1]:
+    if string[0] == string[-1]:
         return pali_check(string[1:len(string)-1])
-    else:
-        return 0
+    return 0
 
+def main():
+    """Print all the palindromes in the dictionary."""
+    word_list = sorted(set(load_dictionary.load('../../dictionary.txt')))
+    pali_list = []
+    for word in word_list:
+        if len(word) > 1 and pali_check(word):
+            pali_list.append(word)
 
-pali_list = []
-for word in word_list:
-    if len(word) > 1 and pali_check(word):
-           pali_list.append(word)
+    print("\nNumber of palindromes found = {}\n".format(len(pali_list)))
+    print(*pali_list, sep='\n')
 
-print("\nNumber of palindromes found = {}\n".format(len(pali_list)))
-print(*pali_list, sep='\n')
+if __name__ == "__main__":
+    main()
