97ce0d05ca73fcf7b84bf46fbdeae8146570d22a
Alan Beavan <alan.beavan@bristol.ac.uk>
2019-11-05 11:36:31 +0000

    added 6

---
+++ 6/projects/blank_line_checker/line_checker.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3.6
+"""Hide a message in a word document."""
+
+import docx
+from docx.shared import RGBColor, Pt
+import my_module as mod
+
+def load_assets(blank_filename, fake_filename, real_filename):
+    """Get all the word doc bits in docx formats."""
+    blank_doc = docx.Document(blank_filename)   
+   
+    fake_message = docx.Document(fake_filename)
+    fake_paragraphs = []
+    for paragraph in fake_message.paragraphs:
+        fake_paragraphs.append(paragraph.text)
+
+    real_message = docx.Document(real_filename)
+    real_paragraphs = []
+    for paragraph in real_message.paragraphs:
+        real_paragraphs.append(paragraph.text)
+
+    return blank_doc, fake_paragraphs, real_paragraphs
+
+def add_letterhead(doc):
+    """Add the initial bits and bobs to the document."""
+    doc.add_heading("Morland Holmes", 0)
+    subtitle = doc.add_heading('Global Consulting & Negotiations', 1)
+    subtitle.alignment = 1
+    doc.add_heading("", 1)
+    doc.add_paragraph("December 17, 2015")
+    doc.add_paragraph("")
+
+def set_spacing(paragraph):
+    """Set line spacing betwween paragraphs (so we can hide the message)."""
+    paragraph_format = paragraph.paragraph_format
+    paragraph_format.space_before = Pt(0)
+    paragraph_format.space_after = Pt(0)
+
+def check_lines(fake, real):
+    """Check if the number of blank lines is enough to hold the message."""
+    length_real = len(real)
+    empty_count = 0
+    for line in fake:
+        if line == "":
+            empty_count += 1
+    if empty_count >= length_real:
+        return 1
+    return 0
+
+def interleave_message(doc, fake, real):
+    """Hide the real message in the fake message.
+
+    Write the fake message in the doc where there are lines.
+    Write the real message where the fake message is just newlines, in white.
+    Save the document.
+    """
+    length_real = len(real)
+    count_real = 0
+
+    for line in fake:
+        if count_real < length_real and line == "":
+            paragraph = doc.add_paragraph(real[count_real])
+            paragraph_index = len(doc.paragraphs) - 1
+            run = doc.paragraphs[paragraph_index].runs[0]
+            font = run.font
+            font.color.rgb = RGBColor(255, 255, 255) #Currently red
+            count_real += 1
+        else:
+            paragraph = doc.add_paragraph(line)
+        set_spacing(paragraph)
+
+    doc.save("ciphertext.docx")
+
+
+def main():
+    """Do the things."""
+    blank_doc, fake_list, real_list = load_assets("template.docx",
+                                                  "fakeMessage.docx",
+                                                  "realMessage_Vig.docx")
+    add_letterhead(blank_doc)
+    if check_lines(fake_list, real_list):
+        interleave_message(blank_doc, fake_list, real_list)
+        print("Done!")
+    else:
+        print("Message too long for the number of blank lines in the fake" \
+              "message")
+    
+if __name__ == "__main__":
+    main()
---
+++ 6/projects/monospace/message_hider.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3.6
+"""Hide a message in a word document.
+
+I couldn't get it not to do funny line breaks, so it requires
+paragraphs of one line or fewer in the fake message.
+"""
+
+import docx
+from docx.shared import RGBColor, Pt
+import my_module as mod
+
+def load_assets(blank_filename, fake_filename, real_filename):
+    """Get all the word doc bits in docx formats."""
+    blank_doc = docx.Document(blank_filename)
+
+    for style in ["Normal", "Heading 1", "Title"]:
+        blank_doc.styles[style].font.name = "Courier"
+    
+    fake_message = docx.Document(fake_filename)
+    fake_paragraphs = []
+    for paragraph in fake_message.paragraphs:
+        fake_paragraphs.append(paragraph.text)
+
+    real_message = docx.Document(real_filename)
+    real_paragraphs = []
+    for paragraph in real_message.paragraphs:
+        real_paragraphs.append(paragraph.text)
+    real = "".join(real_paragraphs)
+
+    return blank_doc, fake_paragraphs, real
+
+def add_letterhead(doc):
+    """Add the initial bits and bobs to the document."""
+    doc.add_heading("Morland Holmes", 0)
+    subtitle = doc.add_heading('Global Consulting & Negotiations', 1)
+    subtitle.alignment = 1
+    doc.add_heading("", 1)
+    doc.add_paragraph("December 17, 2015")
+    doc.add_paragraph("")
+
+def set_spacing(paragraph):
+    """Set line spacing betwween paragraphs (so we can hide the message)."""
+    paragraph_format = paragraph.paragraph_format
+    paragraph_format.space_before = Pt(0)
+    paragraph_format.space_after = Pt(0)
+
+def interleave_message(doc, fake, real):
+    """Hide the real message in the fake message.
+
+    Write the fake message in the doc where there are lines.
+    Write the real message where the fake message is just newlines, in white.
+    Save the document.
+    """
+    length_real = len(real)
+    count_real = 0
+    start = 0
+
+    for line in fake:
+        paragraph = doc.add_paragraph("")
+        copy = list(line)
+        if count_real < length_real:
+            start = 0
+            for i in range(len(line)):
+                if count_real < length_real:
+                    if line[i] == " ":
+                        paragraph.add_run("".join(copy[start:i]))
+                        #change_colour(255, 0, 0)
+                        paragraph.add_run(real[count_real]).font.color.rgb = RGBColor(255, 255, 255)
+                        #change_colour(0, 0, 0)
+                        count_real += 1
+                        start = i+1
+                else:
+                    paragraph.add_run("".join(copy[start:]))
+                    break
+            #paragraph = doc.add_paragraph(real[count_real])
+            #paragraph_index = len(doc.paragraphs) - 1
+            #run = doc.paragraphs[paragraph_index].runs[0]
+            #font = run.font
+            #font.color.rgb = RGBColor(255, 0, 0) #Currently red
+            #count_real += 1
+        else:
+            paragraph.add_run("".join(copy))
+        set_spacing(paragraph)
+    
+    doc.save("ciphertext.docx")
+
+
+def main():
+    """Do the things."""
+    blank_doc, fake_list, real = load_assets("template.docx",
+                                             "fakeMessage.docx",
+                                             "realMessage.docx")
+    add_letterhead(blank_doc)
+    interleave_message(blank_doc, fake_list, real)
+    print("Done!")
+    
+if __name__ == "__main__":
+    main()
